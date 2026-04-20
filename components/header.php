efc84c2deab3e1998330ad0a624b1d6cd858648a
Alexis <2362873@cstj.qc.ca>
2026-04-20 12:47:48 -0400

    hotfix - image header

--- components/header.php
+++ components/header.php
@@ -1,16 +1,17 @@
 <header class="hero">
+    
+    <!-- Icône menu -->
     <span id="menu-toggle" class="material-symbols-outlined menu-icon">
         menu
     </span>
 
-    <!-- seulement >=680px -->
-    <video width="320" height="240" autoplay muted loop id="hero-video">
+    <video autoplay muted loop playsinline id="hero-video">
         <source src="assets/video/video_restaurant_optimise.mp4" type="video/mp4">
-        Votre naviguateur ne supporte pas les vidéos HTML5.
+        Votre navigateur ne supporte pas les vidéos HTML5.
     </video>
 
-    <?php if ($showImg ?? false): ?>
-    <!-- seulement <680px -->
-    <img id="img--background" src="assets/images/png/header_image.jpg" alt="Arrière-plan" />
-    <?php endif; ?>
+    <img 
+        id="img--background" src="assets/images/png/header_image.jpg" alt="Arrière-plan du restaurant" 
+    />
+
 </header>
