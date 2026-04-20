37e914bec8dd4ee3e0377a92f74353b33d0088d0
Alexis <2362873@cstj.qc.ca>
2026-04-20 15:05:28 -0400

    add - images entrees

--- entrees.php
+++ entrees.php
@@ -27,51 +27,75 @@ $showImg = false;
         <div class="menu-list">
 
             <div class="menu-item">
-                <h3>Edamame au sel de mer fumé</h3>
-                <p>Fèves de soja vapeur, sel fumé et zeste de yuzu</p>
-                <span>10$</span>
+                <img src="assets/images/jpg/1_edamame.jpg" alt="Edamame au sel de mer fumé">
+                <div class="menu-content">
+                    <h3>Edamame au sel de mer fumé</h3>
+                    <p>Fèves de soja vapeur, sel fumé et zeste de yuzu</p>
+                    <span>10$</span>
+                </div>
             </div>
 
             <div class="menu-item">
-                <h3>Tataki de thon rouge</h3>
-                <p>Thon saisi, sauce ponzu, gingembre mariné</p>
-                <span>16$</span>
+                <img src="assets/images/jpg/2_tataki_thon_rouge.jpg" alt="Tataki de thon rouge">
+                <div class="menu-content">
+                    <h3>Tataki de thon rouge</h3>
+                    <p>Thon saisi, sauce ponzu, gingembre mariné</p>
+                    <span>16$</span>
+                </div>
             </div>
 
             <div class="menu-item">
-                <h3>Gyoza de porc et crevettes</h3>
-                <p>Raviolis grillés, sauce miso épicée</p>
-                <span>14$</span>
+                <img src="assets/images/jpg/3_gyoza_porc_crevettes.jpg" alt="Gyoza de porc et crevettes">
+                <div class="menu-content">
+                    <h3>Gyoza de porc et crevettes</h3>
+                    <p>Raviolis grillés, sauce miso épicée</p>
+                    <span>14$</span>
+                </div>
             </div>
 
             <div class="menu-item">
-                <h3>Salade wakame et sésame noir</h3>
-                <p>Algues marinées, vinaigrette soja-sésame</p>
-                <span>15$</span>
+                <img src="assets/images/jpg/4_salade_wakame.jpg" alt="Salade wakame et sésame noir">
+                <div class="menu-content">
+                    <h3>Salade wakame et sésame noir</h3>
+                    <p>Algues marinées, vinaigrette soja-sésame</p>
+                    <span>15$</span>
+                </div>
             </div>
 
             <div class="menu-item">
-                <h3>Soupe miso traditionnelle</h3>
-                <p>Bouillon miso, tofu, algues wakame, oignons verts</p>
-                <span>8$</span>
+                <img src="assets/images/jpg/5_soupe_miso.jpg" alt="Soupe miso traditionnelle">
+                <div class="menu-content">
+                    <h3>Soupe miso traditionnelle</h3>
+                    <p>Bouillon miso, tofu, algues wakame, oignons verts</p>
+                    <span>8$</span>
+                </div>
             </div>
 
             <div class="menu-item">
-                <h3>Tempura de crevettes</h3>
-                <p>Pâte croustillante, sauce tentsuyu</p>
-                <span>14$</span>
+                <img src="assets/images/jpg/6_tempura_crevettes.jpg" alt="Tempura de crevettes">
+                <div class="menu-content">
+                    <h3>Tempura de crevettes</h3>
+                    <p>Pâte croustillante, sauce tentsuyu</p>
+                    <span>14$</span>
+                </div>
             </div>
 
             <div class="menu-item">
-                <h3>Tartare de saumon façon japonaise</h3>
-                <p>Saumon, huile de sésame, shiso, tobiko</p>
-                <span>18$</span>
+                <img src="assets/images/jpg/7_tartare_saumon.jpg" alt="Tartare de saumon façon japonaise">
+                <div class="menu-content">
+                    <h3>Tartare de saumon façon japonaise</h3>
+                    <p>Saumon, huile de sésame, shiso, tobiko</p>
+                    <span>18$</span>
+                </div>
             </div>
 
             <div class="menu-item">
-                <h3>Yakitori de poulet</h3>
-                <p>Brochettes de poulet laqué, sauce tare maison</p>
-                <span>16$</span>
+                <img src="assets/images/jpg/8_yakitori_poulet.jpg" alt="Yakitori de poulet">
+                <div class="menu-content">
+                    <h3>Yakitori de poulet</h3>
+                    <p>Brochettes de poulet laqué, sauce tare maison</p>
+                    <span>16$</span>
+                </div>
             </div>
 
         </div>
