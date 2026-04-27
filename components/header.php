239eee8c31424b7b87297bdef109eaa677bf519b
Alexis <2362873@cstj.qc.ca>
2026-04-27 09:29:52 -0400

    add - logo header

--- components/header.php
+++ components/header.php
@@ -5,13 +5,16 @@
         menu
     </span>
 
+    <!-- LOGO -->
+    <img src="assets/images/png/1_logo.png" alt="Logo" class="logo-header">
+
     <video autoplay muted loop playsinline id="hero-video">
         <source src="assets/video/video_restaurant_optimise.mp4" type="video/mp4">
-        Votre navigateur ne supporte pas les vidéos HTML5.
     </video>
 
     <img 
-        id="img--background" src="assets/images/png/header_image.jpg" alt="Arrière-plan du restaurant" 
+        id="img--background" 
+        src="assets/images/png/header_image.jpg" 
+        alt="Arrière-plan du restaurant" 
     />
-
 </header>
