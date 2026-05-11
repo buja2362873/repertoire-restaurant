4c997e3ca012f20df96484fc3b5d8bd5d7825189
Alexis <2362873@cstj.qc.ca>
2026-05-11 10:45:41 -0400

    add - reservation promo scene

--- components/promo.php
+++ components/promo.php
@@ -1,8 +1,15 @@
 <section class="promo">
 
-    <div class="promo-image"></div>
+    <div class="promo-contenu">
 
-    <h2>30% de RABAIS</h2>
-    <p>Ouverture le 1er juin</p>
+        <h2>30% <span>DE RABAIS</span></h2>
+
+        <p class="promo-ouverture">Ouverture le 1er juin</p>
+
+        <a href="reservations.php" class="promo-bouton">
+            Réserver une table
+        </a>
+
+    </div>
 
 </section>
